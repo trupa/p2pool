a5c96e3032e8e2f082a0a1ec30a6a47d51116e4c
trupa <cesar.nettle@gmail.com>
2014-03-14 18:33:37 -0600

    Changed web interface

--- web-static/conf/settings.js
+++ web-static/conf/settings.js
@@ -1,2 +1,14 @@
-var siteTitle = "P2Pool Consortium Pool";
+var siteTitle = "P2Pool Pool";
 var contactAddress = "pool-contact@emailaddy.com";
+var ip = location.hostname;
+var port = location.port;
+
+window.onload = function() {
+       //when the document is finished loading, replace everything
+       //between the <span ...> </span> tags with the value of splitText
+   document.getElementById("ip").innerHTML=ip;
+   document.getElementById("port").innerHTML=port;
+   document.getElementById("ip2").innerHTML=ip;
+   document.getElementById("port2").innerHTML=port;
+   
+} 
